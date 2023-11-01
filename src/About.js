3ffc08ae3e51669f293f51c1867209f137862c31
SATHIYAPRIYAN7 <sathiyapriyan7@gmail.com>
2023-11-01 16:01:47 +0530

    init

--- src/About.js
+++ src/About.js
@@ -6,17 +6,17 @@ function About() {
   const style={
     backgroundColor:"rgb(30,41,59,0.6)",
     minHeight:"100vh",
-    
+    borderRadius:"10px"
   }
   return (
     <div style={style} className='About-div'>
 
       <div style={{display:"flex",justifyContent:"space-between"}}>
-      <h1 style={{fontSize:"25px",fontWeight:"bold",color:"white",}}>About Me</h1>
+      <h1 style={{fontSize:"25px",fontWeight:"bold",color:"white",paddingLeft:"15px",paddingTop:"10px",textDecoration:"underline",textDecorationColor:"yellow",textUnderlineOffset:'10px'}}>About Me</h1>
 
-      <div style={{display:"flex" ,width:"60%",justifyContent:"space-evenly",backgroundColor:"rgb(30,41,59)"}}>
-        <p style={{color:"white",fontWeight:"bold",textDecoration:"underline",textDecorationColor:"yellow"}}>About</p>
-        <p  style={{color:"white"}}><a href={<Certificate />}>Certification</a></p>
+      <div style={{display:"flex" ,width:"60%",justifyContent:"space-evenly",backgroundColor:"rgb(30,41,59)",marginBottom:"40px",borderRadius:"0px 10px 0px 10px"}}>
+        <p style={{color:"white",fontWeight:"bold",textDecoration:"underline",textDecorationColor:"yellow",marginBottom:'15px'}}>About</p>
+        <p  style={{color:"white"}}><a >Certification</a></p>
         <p style={{color:"white"}}>Projects</p>
         <p style={{color:"white"}}>More</p>
         <p style={{color:"white"}}>Contact</p>
@@ -34,8 +34,8 @@ function About() {
 
       <div style={{display:"flex",justifyContent:"space-between",flexWrap:"wrap"}}>
 
-        <div style={{width:"400px",backgroundColor:"rgb(30,41,59)",margin:"30px"}}>
-        <div style={{display:"flex",width:"90%",justifyContent:"space-between"}}>
+        <div style={{width:"400px",backgroundColor:"rgb(30,41,59,0.8)",margin:"30px",borderRadius:"10px"}}>
+        <div style={{display:"flex",width:"90%",justifyContent:"space-between",margin:"5px 0px"}}>
             <div style={{paddingLeft:"10px"}}>icon</div>
             <div style={{paddingLeft:"100px"}}>
             <p>Web Design & Development</p>
@@ -43,31 +43,31 @@ function About() {
             </div>
         </div></div>
 
-        <div style={{width:"400px",backgroundColor:"rgb(30,41,59)",margin:"30px"}}>
-        <div style={{display:"flex",width:"90%",justifyContent:"space-between"}}>
+        <div style={{width:"400px",backgroundColor:"rgb(30,41,59,0.8)",margin:"30px",borderRadius:"10px"}}>
+        <div style={{display:"flex",width:"90%",justifyContent:"space-between",margin:"5px 0px"}}>
             <div style={{paddingLeft:"10px"}}>icon</div>
             <div style={{paddingLeft:"100px"}}>
-            <p>Web Design & Development</p>
-            <p>Creating attractive,easy to use websites that work well for businesses and individuals online.</p>
+            <p>Mobile Apps & Games</p>
+            <p>Creating and developing engaging mobile apps and games for iOS and Android devices.</p>
             </div>
         </div></div>
 
 
-        <div style={{width:"400px",backgroundColor:"rgb(30,41,59)",margin:"30px"}}>
-        <div style={{display:"flex",width:"90%",justifyContent:"space-between"}}>
+        <div style={{width:"400px",backgroundColor:"rgb(30,41,59,0.8)",margin:"30px",borderRadius:"10px"}}>
+        <div style={{display:"flex",width:"90%",justifyContent:"space-between",margin:"5px 0px"}}>
             <div style={{paddingLeft:"10px"}}>icon</div>
             <div style={{paddingLeft:"100px"}}>
-            <p>Web Design & Development</p>
-            <p>Creating attractive,easy to use websites that work well for businesses and individuals online.</p>
+            <p>Development Solutions</p>
+            <p>Utilizing JavaScript frameworks like React, Node, Next, Vue and more to build interactive web applications.</p>
             </div>
         </div></div>
 
-        <div style={{width:"400px",backgroundColor:"rgb(30,41,59)",margin:"30px"}}>
-        <div style={{display:"flex",width:"90%",justifyContent:"space-between"}}>
+        <div style={{width:"400px",backgroundColor:"rgb(30,41,59,0.8)",margin:"30px",borderRadius:"10px"}}>
+        <div style={{display:"flex",width:"90%",justifyContent:"space-between",margin:"5px 0px"}}>
             <div style={{paddingLeft:"10px"}}>icon</div>
             <div style={{paddingLeft:"100px"}}>
-            <p>Web Design & Development</p>
-            <p>Creating attractive,easy to use websites that work well for businesses and individuals online.</p>
+            <p>Marketing & SEO</p>
+            <p>Using SEO and marketing strategies to boost online visibility and promote products or services.</p>
             </div>
         </div></div>
         
@@ -85,26 +85,26 @@ function About() {
 
 
      <div style={{color:"white"}}> 
-        <h1 style={{textAlign:"center",fontSize:"28px",fontWeight:"bold",paddingTop:"20px"}}>Primary Focus</h1>
+        <h1 style={{textAlign:"center",fontSize:"28px",fontWeight:"bold",paddingTop:"20px"}}>Exploring Creativity</h1>
       
 
       <div style={{display:"flex",justifyContent:"space-between",flexWrap:"wrap"}}>
 
-        <div style={{width:"400px",backgroundColor:"rgb(30,41,59)",margin:"30px"}}>
-        <div style={{display:"flex",width:"90%",justifyContent:"space-between"}}>
+        <div style={{width:"400px",backgroundColor:"rgb(30,41,59,0.8)",margin:"30px",borderRadius:"10px"}}>
+        <div style={{display:"flex",width:"90%",justifyContent:"space-between",margin:"5px 0px"}}>
             <div style={{paddingLeft:"10px"}}>icon</div>
             <div style={{paddingLeft:"100px"}}>
-            <p>Web Design & Development</p>
-            <p>Creating attractive,easy to use websites that work well for businesses and individuals online.</p>
+            <p>Visual Storytelling</p>
+            <p>Taking and editing photos or videos that tell engaging stories, bringing concepts and ideas to life visually.</p>
             </div>
         </div></div>
 
-        <div style={{width:"400px",backgroundColor:"rgb(30,41,59)",margin:"30px"}}>
-        <div style={{display:"flex",width:"90%",justifyContent:"space-between"}}>
+        <div style={{width:"400px",backgroundColor:"rgb(30,41,59,0.8)",margin:"30px",borderRadius:"10px"}}>
+        <div style={{display:"flex",width:"90%",justifyContent:"space-between",margin:"5px 0px"}}>
             <div style={{paddingLeft:"10px"}}>icon</div>
             <div style={{paddingLeft:"100px"}}>
-            <p>Web Design & Development</p>
-            <p>Creating attractive,easy to use websites that work well for businesses and individuals online.</p>
+            <p>Digital Content Creation</p>
+            <p>Creatiing dynamic digital content, such as 3D models, graphics or animations.</p>
             </div>
         </div></div>
 
